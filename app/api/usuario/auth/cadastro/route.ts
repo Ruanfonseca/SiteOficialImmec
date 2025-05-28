9b145ce63b8cdc74716b5f8f894d970530e46acf
Ruan Fonseca <whitelook22@outlook.com>
2025-05-28 18:14:22 -0300

    criando correção no route

--- app/api/usuario/auth/cadastro/route.ts
+++ app/api/usuario/auth/cadastro/route.ts
@@ -1,20 +1,23 @@
-import { NextResponse } from 'next/server';
-import { prisma } from '@/lib/prisma';
-import { hashPassword } from '@/lib/auth';
+
+
+// export async function POST(req: Request) {
+//   const { email, password, name } = await req.json();
+
+//   const existingUser = await prisma.user.findUnique({ where: { email } });
+//   if (existingUser) {
+//     return NextResponse.json({ error: 'Email já registrado.' }, { status: 400 });
+//   }
+
+//   const hashed = hashPassword(password);
+
+//   const user = await prisma.user.create({
+//     data: { email, password: hashed, name }
+//   });
+
+//   return NextResponse.json({ message: 'Usuário registrado com sucesso', user: { id: user.id, email: user.email } });
+// }
+
 
 export async function POST(req: Request) {
-  const { email, password, name } = await req.json();
 
-  const existingUser = await prisma.user.findUnique({ where: { email } });
-  if (existingUser) {
-    return NextResponse.json({ error: 'Email já registrado.' }, { status: 400 });
-  }
-
-  const hashed = hashPassword(password);
-
-  const user = await prisma.user.create({
-    data: { email, password: hashed, name }
-  });
-
-  return NextResponse.json({ message: 'Usuário registrado com sucesso', user: { id: user.id, email: user.email } });
 }
